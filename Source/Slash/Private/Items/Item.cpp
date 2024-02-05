8f25069bc3378246648de3b4f6c2b602e3b8888d
MinjongKim <minjong0524@gmail.com>
2024-02-05 15:27:03 +0900

    Create a project

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -12,16 +12,12 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("BeginePlay"));
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	for (int32 index = 0; index <= 10; ++index)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Tick"));
-	} 
 }
 
